be7215667e91a05cfe4c7b61801a3314183be1fd
Jeremiah Towe <jeremiah@shootproof.com>
2025-02-22 14:48:34 -0500

    fix tests locally

--- cypress/e2e/monsters.cy.ts
+++ cypress/e2e/monsters.cy.ts
@@ -1,6 +1,8 @@
 describe('Monster App', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
+    // Wait for the grid to be populated with monsters
+    cy.get('.grid').children().should('have.length.gt', 0)
   })
 
   it('loads the initial page', () => {
@@ -9,21 +11,34 @@ describe('Monster App', () => {
   })
 
   it('can search for monsters', () => {
-    cy.get('input[placeholder="Search monsters..."]').type('orc')
-    // Wait for search results
-    cy.get('.grid').children().should('have.length.gt', 0)
+    // Get a monster name from the statblock title
+    cy.get('.grid h1').first().invoke('text').then((name) => {
+      // Take first 3 characters of the name to search
+      const searchTerm = name.slice(0, 3).toLowerCase()
+
+      // Type the search term
+      cy.get('input[placeholder="Search monsters..."]').type(searchTerm)
+
+      // Wait for filtered results and verify they contain the search term
+      cy.get('.grid').children().should('exist')
+      cy.get('.grid h1').each(($el) => {
+        expect($el.text().toLowerCase()).to.include(searchTerm)
+      })
+    })
   })
 
   it('can select and deselect monsters', () => {
-    // Select first monster
-    cy.get('.grid > div:first-child input[type="checkbox"]').click()
+    // Wait for monsters to load and select first one
+    cy.get('.grid').children().first().within(() => {
+      cy.get('input[type="checkbox"]').click()
+    })
 
     // Wait for the selected monsters list to update
     cy.contains('Selected Monsters')
       .parent()
-      .parent()  // Go up one more level to find the container
-      .find('ul')  // Find the ul that contains the list items
-      .find('li')  // Then find the li elements
+      .parent()
+      .find('ul')
+      .find('li')
       .should('have.length', 1)
 
     // Deselect using X button
@@ -39,8 +54,10 @@ describe('Monster App', () => {
   })
 
   it('shows print button when monsters are selected', () => {
-    // Select first monster
-    cy.get('.grid > div:first-child input[type="checkbox"]').click()
+    // Wait for monsters to load and select first one
+    cy.get('.grid').children().first().within(() => {
+      cy.get('input[type="checkbox"]').click()
+    })
     cy.get('button[title="Print Selected Monsters"]').should('exist')
   })
 
@@ -49,7 +66,8 @@ describe('Monster App', () => {
     cy.get('label').contains('Show Images')
       .find('input[type="checkbox"]').click()
 
-    // If there are monsters with images, they should be visible
-    cy.get('.grid').find('img').should('exist')
+    // Wait for monsters and then check for images
+    cy.get('.grid').children().should('have.length.gt', 0)
+    cy.get('.grid').find('.monster-image-container').should('exist')
   })
 })
