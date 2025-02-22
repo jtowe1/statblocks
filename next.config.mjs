90ce94fbc544edc737aaa1f9d4db11848bab342c
Jeremiah Towe <jeremiah@shootproof.com>
2025-02-22 14:42:32 -0500

    fix tests locally

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,16 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true, // We'll handle TypeScript errors separately
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media-waterdeep.cursecdn.com',
+        pathname: '/**/*', // Allow all paths under this domain
+      },
+    ],
+  },
 };
 
 export default nextConfig;
